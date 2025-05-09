06458d3dd667132d1e8dbfdba56e4cd9d09595ef
nafis200 <nafisahamed14@gmail.com>
2025-05-09 19:41:09 +0600

    vector

---
+++ struct.cpp
@@ -0,0 +1,14 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+struct point {
+    int x,y;
+}p,q;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    // point p;
+    p.x = 10;
+    p.y = 20;
+    cout << p.x << " " << p.y << "\n";
+}
