c1964c58a6a82aa9f906d9b3c6bd3afcecd4dac9
nafis200 <nafisahamed14@gmail.com>
2025-05-09 19:30:57 +0600

    first commit

---
+++ pair.cpp
@@ -0,0 +1,13 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    pair<int,int>point;
+    point = make_pair(0, 3);
+    pair<int,int>p1,p2;
+    p1 = make_pair(0,3);
+    p2= make_pair(1,5);
+    swap(p1,p2);
+}
